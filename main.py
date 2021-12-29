f87fa925736aad8a48be3b8e25f34e3107617ac7
Pmoua015 <paul.moua2014@gmail.com>
2021-12-29 14:10:40 -0600

    data

--- main.py
+++ main.py
@@ -1,131 +1,149 @@
-import time
+import apache_beam as beam
+from google.cloud import bigquery
+import ast
 import json
-import numpy
-import names
-import random
-from faker import Faker
-from datetime import datetime
-from google.cloud import pubsub_v1
 
-# pub/sub values
-PROJECT_ID = "york-cdf-start"
-TOPIC = "dataflow-project-orders"
+SUBSCRIPTION_ID = "projects/york-cdf-start/subscriptions/pm_topic-sub" # my pub/sub
+n_sub = "projects/york-cdf-start/subscriptions/dataflow-order-stock-update-sub" # sub for the project
+n_topic = "projects/york-cdf-start/topics/dataflow-order-stock-update" # topic for the project
+# BQ_Table = "york-cdf-start:p_moua_proj_1.testing" # testing table that stored data
+USD_table = "york-cdf-start:p_moua_proj_1.usd_order_payment_history" # created table in bigquery
+EUR_table = "york-cdf-start:p_moua_proj_1.eur_order_payment_history"
+GBP_table = "york-cdf-start:p_moua_proj_1.gbp_order_payment_history"
+currency = ["USD", "EUR", "GBP"] # put the currency into an array
 
-# range values
-order_frequency_range = {'min': 1, 'max': 5, 'step': 1}
-product_count_range = {'min': 1, 'max': 5, 'step': 1}
-product_price_range = {'min': 50, 'max': 5000, 'step': 0.01}
-cost_tax_range = {'min': 5, 'max': 50, 'step': 0.01}
-cost_shipping_range = {'min': 5, 'max': 50, 'step': 0.01}
+# schema for how the table should be formatted
+table_schema = {
+    'fields': [
+        {"name": "order_id", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "order_address", "type": "RECORD", "mode": "REPEATED",
+         'fields': [
+             {"name": "order_building_number", "type": "STRING", "mode": "NULLABLE"},
+             {"name": "order_street_name", "type": "STRING", "mode": "NULLABLE"},
+             {"name": "order_city", "type": "STRING", "mode": "NULLABLE"},
+             {"name": "order_state_code", "type": "STRING", "mode": "NULLABLE"},
+             {"name": "order_zip_code", "type": "STRING", "mode": "NULLABLE"}],
+         },
+        {"name": "order_currency", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "customer_first_name", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "customer_last_name", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "customer_ip", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "customer_time", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "cost_total", "type": "FLOAT", "mode": "NULLABLE"}]
 
-# init
-fake = Faker()
-publisher = pubsub_v1.PublisherClient()
-topic_path = publisher.topic_path(PROJECT_ID, TOPIC)
+}
 
+# where all the data was transformed/decoded and put into a dictionary in order to read
+class SplitName(beam.DoFn):
+    new_dict = {}
 
-def generate_order():
-    customer_name = names.get_full_name()
-    customer_time = datetime.now().strftime('%d/%b/%Y:%H:%M:%S')
-    customer_ip = fake.ipv4()
+    # generator
+    def process(self, element):
+        # finding sum of price, cost_shipping & cost_tax, price & id were in its own array
+        order_items = element["order_items"]
+        price = 0
+        item_list = []
+        i = 0
+        while i < len(order_items):
+            price += float(order_items[i]["price"])
+            item_list.append(order_items[i]["id"])
+            i += 1
+        cost_total = sum([price, float(element["cost_shipping"]), float(element["cost_tax"])])
 
-    order_id = fake.postcode()
-    order_currency = random.choice(["USD", "EUR", "GBP"])
-    order_address = fake.address().replace('\n', ', ')
-    order_items = generate_customer_cart()
+        # splitting variables away from their element []
+        customer_first_name = element["customer_name"].split()[0]
+        customer_last_name = element["customer_name"].split()[1]
+        order_building_number = element["order_address"].split()[0]
+        street_name = (element["order_address"].split(",")[0]).split()[1:]
+        order_street_name = " ".join(street_name)
+        order_city = element["order_address"].split(",")[1]
+        order_state_code = (element["order_address"].split(",")[-1]).split()[-2]
+        order_zip_code = (element["order_address"].split()[-1])
 
-    cost_shipping = round(
-        random.choice(
-            numpy.arange(
-                cost_shipping_range['min'],
-                cost_shipping_range['max'],
-                cost_shipping_range['step']
-            )
-        ),
-        2
-    )
-    cost_tax = round(
-        random.choice(
-            numpy.arange(
-                cost_tax_range['min'],
-                cost_tax_range['max'],
-                cost_tax_range['step']
-            )
-        ),
-        2
-    )
-
-    return {
-        'customer_name': customer_name,
-        'customer_time': customer_time,
-        'customer_ip': customer_ip,
-
-        'order_id': order_id,
-        'order_currency': order_currency,
-        'order_address': order_address,
-        'order_items': order_items,
-
-        'cost_shipping': cost_shipping,
-        'cost_tax': cost_tax
-    }
-
-
-def generate_customer_cart():
-    product_list = []
-    product_count = random.choice(
-        range(
-            product_count_range['min'],
-            product_count_range['max'],
-            product_count_range['step']
-        )
-    )
-
-    # generate customer cart
-    for i in range(product_count):
-        product_list.append({
-            'id': fake.postcode(),
-            'name': names.get_last_name(),
-            'price': round(
-                random.choice(
-                    numpy.arange(
-                        product_price_range['min'],
-                        product_price_range['max'],
-                        product_price_range['step']
-                    )
-                ),
-                2
-            )
-        })
-
-    return product_list
+        # the new dictionary that holds my information, how i want it to look when printed
+        new_dict = {
+            "order_id": element["order_id"],
+            "order_address": [{"order_building_number": order_building_number,
+                              "order_street_name": order_street_name,
+                              "order_city": order_city,
+                              "order_state_code": order_state_code,
+                              "order_zip_code": order_zip_code}],
+            "order_currency": element["order_currency"],
+            "customer_ip": element["customer_ip"],
+            "customer_first_name": customer_first_name,
+            "customer_last_name": customer_last_name,
+            "customer_time": element["customer_time"],
+            "cost_total": cost_total
+        }
+        # print(new_dict)
+        yield new_dict # yield is used like return however the function returns a generator
 
 
-def publish(publisher, topic, message):
-    data = json.dumps(message).encode('utf-8')
-    return publisher.publish(topic_path, data=data)
+# followed the partition example from apache beam documentation to split USD, EUR, & GBP from list
+def by_currency(element, num_currency):
+    return currency.index(element['order_currency'])
+
+# this function helps me run the message to display order_list & item_list; got the idea from previous generator
+class create_message(beam.DoFn):
+    message = {}
+    def process(self, element):
+        all_items = element["order_items"]
+        item_list = []
+        i = 0
+        while i < len(all_items):
+            item_list.append(all_items[i]["id"])
+            i += 1
+        message = {
+            "order_id": element["order_id"],
+            "item_list": item_list
+            }
+        yield message
+
+# prints the converted data
+class PrintValue(beam.DoFn):
+    def process(self, element):
+        element = element.decode()
+        element = ast.literal_eval(element)
+        # print(element)
+        return [element]
 
 
-def callback(message_future):
-    # When timeout is unspecified, the exception method waits indefinitely.
-    if message_future.exception(timeout=30):
-        print('Publishing message on {} threw an Exception {}.'.format(message_future.exception()))
-    else:
-        print(message_future.result())
+def run():
+    o = beam.options.pipeline_options.PipelineOptions(streaming=True, save_main_session=True)
+    with beam.Pipeline(options=o) as pipeline:
+        raw_data = pipeline | 'Read in Message' >> beam.io.ReadFromPubSub(subscription=SUBSCRIPTION_ID)
+        conv_data = raw_data | 'Print' >> beam.ParDo(PrintValue())
 
+        # writes my message to pubsub & displays the output with beam.Map(print)
+        m_psub = conv_data | 'Create Pubsub Output' >> beam.ParDo(create_message())
+        conv_message = m_psub | 'Convert Message from Dict To Bytestring' >> beam.Map(lambda s: json.dumps(s).encode("utf-8"))
+        conv_message | 'Publish data to Pubsub' >> beam.io.WriteToPubSub(topic=n_topic)
+        conv_message | beam.io.ReadFromPubSub(subscription=n_sub) | beam.Map(print) # displays converted message to see if message displays correctly
 
-if __name__ == '__main__':
-    while True:
-        order = generate_order()
-        message_future = publish(publisher, topic_path, order)
-        message_future.add_done_callback(callback)
-
-        timeout_seconds = random.choice(
-            range(
-                order_frequency_range['min'],
-                order_frequency_range['max'],
-                order_frequency_range['step']
-            )
+        # partitions my converted data
+        usd, eur, gbp = (
+                        conv_data | 'Partition' >> beam.Partition(by_currency, len(currency))
         )
 
-        print(order)
-        time.sleep(timeout_seconds)
+        # converts each partitioned data into the format that I want it
+        t_USD = usd | 'Transform USD' >> beam.ParDo(SplitName())
+        t_EUR = eur | 'Transform EUR' >> beam.ParDo(SplitName())
+        t_GBP = gbp | 'Transform GBP' >> beam.ParDo(SplitName())
+
+        # writes each currency into it's own table
+        t_USD | 'Write to USD_table' >> beam.io.WriteToBigQuery(
+                            USD_table,
+                            schema=table_schema,
+                            create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
+        t_EUR | 'Write to EUR_table' >> beam.io.WriteToBigQuery(
+                            EUR_table,
+                            schema=table_schema,
+                            create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
+        t_GBP | 'Write to GBP_table' >> beam.io.WriteToBigQuery(
+                            GBP_table,
+                            schema=table_schema,
+                            create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
+
+
+if __name__ == "__main__":
+    run()
